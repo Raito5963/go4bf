bf83ada76263fa75d7e5e61deaabe0ae5a2f5398
Raito5963 <raito.5963.co@gmail.com>
2026-06-17 10:00:55 +0900

    create bf compiler

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Raito5963/go4bf
+
+go 1.26.3
---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main(){
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, "usage: bf <file.bf>")
+		os.Exit(1)
+	}
+
+	code, err := os.ReadFile(os.Args[1])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+
+	if err := compile(code); err != nil {
+		fmt.Fprintf(os.Stderr, "runtime error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func compile(code []byte) error {
+	arr := make([]byte, 30000)
+	ptr := 0
+	brackets := mapBrackets(string(code))
+	for pc := 0; pc < len(code); pc++ {
+		switch code[pc] {
+		case '>': ptr++
+		case '<': ptr--
+		case '+': arr[ptr]++
+		case '-': arr[ptr]--
+		case '.': fmt.Printf("%c",arr[ptr])
+		case ',': fmt.Scanf("%c",&arr[ptr])
+		case '[':
+			if arr[ptr] == 0 {
+				pc = brackets[pc]
+			}
+		case ']':
+			if arr[ptr] != 0 {
+				pc = brackets[pc]
+			}
+		case ' ', '\t', '\n', '\r':
+			continue
+		default:
+			return fmt.Errorf("%c is not exist.", code[pc])
+		}
+	}
+	return nil
+}
+
+func mapBrackets(program string) map[int]int {
+	stack := []int{}
+	pairs := make(map[int]int)
+
+	for i, ch := range program {
+		if ch == '[' {
+			stack = append(stack, i)
+		} else if ch == ']' {
+			open := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			pairs[open] = i
+			pairs[i] = open
+		}
+	}
+	return pairs
+}
